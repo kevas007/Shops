c6efb81e2765d0cc4ea9dad8536b8cc80e121202
kevas007 <kevassiobo@gmail.com>
2022-11-20 12:24:46 +0100

    product

--- petnet/store/models.py
+++ petnet/store/models.py
@@ -10,3 +10,15 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     def __str__(self) :
         return  self.tile
+
+class Product(models.Model):
+    user = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE,)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    tile = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description= models.TextField(blank=True, max_length=255)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at=  models.DateTimeField(auto_now=True)
+    def __str__(self) :
+        return  self.tile
